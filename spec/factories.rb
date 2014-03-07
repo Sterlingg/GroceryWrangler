072f047dced76d10a52c98733c6e4d1083b9f34a
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-07 01:20:51 -0800

    Added factories for the receipt models.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,36 @@
+require 'faker'
+
 FactoryGirl.define do
-  factory :user do
-    name     "Joe Joe"
-    email    "joe@joe.joe"
+  factory :category do
+    name     "PoTayToes"
+  end
+
+  factory :item do
+    name "Spaghetti"
+    description Faker::Lorem.paragraph
+    upc Faker::Number.number(12)
+    price 8.95
+    weight 0.500
+    volume 0.0
+    category
+    receipt
   end
 
   factory :store do
-    name     "Safeway"
+    name     Faker::Company.name
   end
+
+  factory :receipt do
+    date_purchased 1.year.from_now
+    notes Faker::Lorem.paragraph
+    total 123.45
+    store
+    user
+  end
+
+  factory :user do
+    name     Faker::Internet.user_name
+    email    Faker::Internet.email
+  end
+
 end
