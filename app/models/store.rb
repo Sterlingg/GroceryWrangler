0e4600f034405d760b48b1be96656eb09f15b478
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-06 19:55:58 -0800

    Establishing has many relationships to receipts for the Store and User model.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
+  has_many :receipts
   validates :name, presence: true, length: {maximum: 50}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :receipts
   validates :name,  presence: true, length: {maximum: 20}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[A-Za-z\d\-.]+\.[a-zA-Z]+\z/
   validates :email, presence: true, length: {maximum: 50}, format: {with: VALID_EMAIL_REGEX}
