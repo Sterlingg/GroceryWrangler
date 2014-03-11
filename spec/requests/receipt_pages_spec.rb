004d9ef09a25ea0f15afdc12f63457bec61b1aa5
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-11 07:23:41 -0700

    Tests for the showing receipts

--- spec/requests/receipt_pages_spec.rb
+++ spec/requests/receipt_pages_spec.rb
@@ -12,8 +12,26 @@ describe "ReceiptPages" do
       expect(page).to have_content('Receipts')
     end
 
-    it "should have a image to add receipts" do
-      page.should have_selector("img[src$='assets/add_receipt.png']")
-    end
+  describe "Show" do
+    let!(:receipt) { FactoryGirl.create(:receipt) }
+    let!(:category) { FactoryGirl.create(:category) }
+    before do 
+      FactoryGirl.create(:item, receipt: receipt, upc: 123456789101, price: 8.95, weight: 0.01, volume: 0.12, name: 'Spaghetti', category: category)
+      visit receipt_path(receipt)
+end
+    subject{ page }
+    
+    it { should have_content 'Name' }
+    it { should have_content 'Category' }
+    it { should have_content 'Weight' }
+    it { should have_content 'Volume' }
+    it { should have_content 'Price' }
+    it { should have_content 'Add Item' }
+    it { should have_content receipt.total }
+    it { should have_content receipt.items.first.name }
+    it { should have_content receipt.items.first.category.name }
+    it { should have_content receipt.items.first.weight }
+    it { should have_content receipt.items.first.volume }
+    
   end
 end
