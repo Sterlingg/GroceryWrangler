8840903490ed02520c1b1a80a80a3d989375b042
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-11 07:18:47 -0700

    Fill in the show method.

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,6 +1,15 @@
 class ReceiptsController < ApplicationController
 
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+
   def index
+    @receipts = Receipt.all()
+  end
+
+  def show
+    @receipt = Receipt.find(params[:id])
   end
 
 end
