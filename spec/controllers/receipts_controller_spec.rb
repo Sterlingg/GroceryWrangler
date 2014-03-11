ed1053b8ecf30706e1c57aa62ade9fa494fffe85
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-11 07:20:50 -0700

    Test that there is an index page for the receipts controller.

--- spec/controllers/receipts_controller_spec.rb
+++ spec/controllers/receipts_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ReceiptsController do
+  describe "GET index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
 
 end
