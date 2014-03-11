a742c05aa14154c00fcbcf09a5e3b7578e82437b
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-11 07:22:15 -0700

    Tests for home page links.

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -4,26 +4,27 @@ describe "UserPages" do
   describe "Index" do
     before{visit root_path}
 
-    it "should have the content Receipts" do
-      expect(page).to have_content('Receipts')
+    it "should have the right links on the layout" do
+      visit root_path
+
+      click_link "Receipts"
+      expect(page).to have_title('Receipts')
+
+      click_link "Recipes"
+      expect(page).to have_title('Recipes')
+
+      click_link "Price Trends"
+      expect(page).to have_title('Price Trends')
+
+      click_link "Budgeting"
+      expect(page).to have_title('Budgeting')
     end
 
-    it "should have the content Price Trends" do
-      expect(page).to have_content('Price Trends')
-    end
-
-    it "should have the content Recipes" do
-      expect(page).to have_content('Recipes')
-    end
-
-    it "should have the content Login" do
-      expect(page).to have_content('Login')
-    end
     it "should have an input box for a username" do
       expect(page).to have_css('input[type="text"]')
     end
-
-    it "should have an input box for the password" do
+    
+    it  "should have an input box for the password" do
       expect(page).to have_css('input[type="password"]')
     end
   end
