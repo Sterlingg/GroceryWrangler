a46d2a45bc27e1662c91829d67fc55f824214711
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-17 16:11:03 -0700

    Routes for modals.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 JoseCanseco::Application.routes.draw do
+  get "modal/category_selection"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
