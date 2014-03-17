b042c5acc510f9e9e41ce1a594daa3abf6361fe4
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-17 16:10:37 -0700

    Test that the right layout is being used.

--- spec/controllers/receipts_controller_spec.rb
+++ spec/controllers/receipts_controller_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe ReceiptsController do
+
+    let!(:receipt) { FactoryGirl.create(:receipt) }
+    let!(:category) { FactoryGirl.create(:category) }
+
+    before do 
+      store_item = FactoryGirl.create(:store_item, upc: 123456789101, price: 8.95, weight: 0.01, volume: 0.12, name: 'Spaghetti', category: category)
+      FactoryGirl.create(:receipt_item, store_item: store_item, receipt: receipt)
+
+      visit receipt_path(receipt)
+    end
+
   describe "GET index" do
+
+    before { get :index }
+
     it "renders the index template" do
-      get :index
       expect(response).to render_template("index")
     end
-  end
 
+    it "should render the application template" do
+      expect(response).to render_template("layouts/application")
+    end
+  end 
 end
