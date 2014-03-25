14880a9e650b0f9d03c79cb588bf4924f9a8f0c6
Sterling Graham <sterlingrgraham@gmail.com>
2014-03-25 14:39:07 -0700

    Notes.

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,4 +1,5 @@
 class Receipt < ActiveRecord::Base
+  # TODO: Make sure a receipt NEVER has multiple of the same store item.
   belongs_to :store
   belongs_to :user
   has_many :receipt_items, dependent: :destroy
