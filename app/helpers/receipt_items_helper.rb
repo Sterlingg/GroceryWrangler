737189d5479f5c7b74f21daba2bdeb45742d6972
Sterling Graham <sterlingrgraham@gmail.com>
2014-04-02 18:15:36 -0700

    Add a button for deleting a receipt item.

--- app/helpers/receipt_items_helper.rb
+++ app/helpers/receipt_items_helper.rb
@@ -1,17 +1,27 @@
 module ReceiptItemsHelper
+
+  def cancel_edit_button()
+    class_string = "cancel-edit-item-btn hide btn btn-primary btn-sm btn-danger glyphicon glyphicon-ban-circle"
+    button_tag("", type: "button", class: class_string)
+  end
+
   def edit_button()
-    class_string = "btn btn-primary btn-sm edit-item-btn glyphicon glyphicon-edit"
+    class_string = "edit-item-btn btn btn-primary btn-sm glyphicon glyphicon-edit"
 
     button_tag("", type: "button", class: class_string)
   end
 
-  def cancel_edit_button()
-    class_string = "hide btn btn-primary btn-sm btn-danger cancel-edit-item-btn glyphicon glyphicon-ban-circle"
-    button_tag("", type: "button", class: class_string)
+  def delete_button(id)
+    class_string = "delete-item-btn btn btn-primary btn-sm btn-danger glyphicon glyphicon-remove pull-right"
+    link_to("", 
+            receipt_item_path(id), 
+            class: class_string, 
+            method: :delete, 
+            remote: true)
   end
 
   def save_button()
-    class_string = "hide btn btn-primary btn-sm save-item-btn glyphicon glyphicon-floppy-disk"
+    class_string = "save-item-btn hide btn btn-primary btn-sm glyphicon glyphicon-floppy-disk"
     button_tag("", class: class_string)
   end
 end
