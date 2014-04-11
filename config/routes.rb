f757d233b80cf63e93becf84e228146677857131
Sterling Graham <sterlingrgraham@gmail.com>
2014-04-10 18:13:48 -0700

    Clean up the routes, and a whitespace change.

--- config/routes.rb
+++ config/routes.rb
@@ -6,73 +6,23 @@ JoseCanseco::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
-  resources :users
+  resources :users, only [:create, :index, :new]
+  resources :store_items, only: [:create, :new, :show]
+  resources :sessions, only: [:create, :destroy, :new]
+  resources :receipt_items, only: [:destroy]
   resources :receipts
   resources :categories
-  resources :receipt_items, only: [:destroy]
-  resources :store_items, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup',  to: 'users#new',         via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/signup',  to: 'users#new',        via: 'get'
+  match '/signin',  to: 'sessions#new',     via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
-  match '/budgeting',    to: 'budgeting#index',    via: 'get'
-  match '/price_trend',   to: 'price_trend#index',   via: 'get'
-  match '/recipes', to: 'recipe#index', via: 'get'
+  match '/budgeting',    to: 'budgeting#index',                          via: 'get'
+  match '/price_trend',   to: 'price_trend#index',                       via: 'get'
+  match '/recipes', to: 'recipe#index',                                  via: 'get'
   match '/category_selection_dialog', to: 'categories#selection_dialog', via: 'get'
 
   match '/receipt_add_items', to: 'receipts#add_items', via: 'post'
-
   match '/receipt_items_selection_dialog', to: 'receipt_items#selection_dialog', via: 'get'
   match '/receipt_items', to: 'receipt_items#update', via: 'patch'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
