15aef766e8e925feafffef341e3cdcfa09a1b547
Sterling Graham <sterlingrgraham@gmail.com>
2014-04-11 22:53:17 -0700

    Delete button html helper.

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -15,4 +15,12 @@ module ReceiptsHelper
 
     link_to(span + "Add Item", category_selection_dialog_path(receipt: @receipt), link_to_opts)
   end
+
+  def delete_receipt_btn(id)
+    span = content_tag(:span, 
+                       "", 
+                       class: "glyphicon glyphicon-remove")
+    class_string = "edit-item-btn btn btn-danger btn-primary btn"
+    link_to(span + " Delete", receipt_path(id), method: :delete, type: "button", class: class_string, confirm: 'Are you sure you want to delete this receipt?')
+  end
 end
