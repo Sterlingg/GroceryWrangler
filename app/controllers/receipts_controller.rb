38e7b26d3e485ecb45d0bdf0b314b3eaf48f1372
Sterling Graham <sterlingrgraham@gmail.com>
2014-05-17 09:28:10 -0700

    Extracted some of the add_items method into the Receipt model.

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -9,25 +9,12 @@ class ReceiptsController < ApplicationController
     @new_receipt_items = []
     @items_to_update = []
 
-    @store_items.each do |store_item|
-      same_receipt_item = get_receipt_item(@receipt, store_item)
-      if same_receipt_item
-        same_receipt_item.quantity += params["quantity_item_#{store_item.id}"].to_f
-
-        same_receipt_item.save!
-        @items_to_update.append({item_id: same_receipt_item.id, quantity: same_receipt_item.quantity})
-      else
-        @new_receipt_item = ReceiptItem.new(quantity: params["quantity_item_#{store_item.id}"], price: params["store_item_price_#{store_item.id}"], store_item: store_item, receipt: @receipt)
-
-        if @new_receipt_item.valid?
-          @new_receipt_items.push(@new_receipt_item) 
-        else
-          respond_to do |format|
-            format.js { render 'error' and return }
-          end
-        end
+    unless @receipt.update_quantity_and_price(@store_items, @items_to_update, @new_receipt_items, params)
+        respond_to do |format|
+        format.js { render 'error' and return }
       end
     end
+
     # This should be safe as the quantity in the database must be a number, and 
     # item_id is gathered from the database itself.
     @items_to_update = @items_to_update.to_json.html_safe
@@ -79,24 +66,6 @@ class ReceiptsController < ApplicationController
   end
 
   private
-  # Given a store_item it looks through the receipt for a receipt_item that has
-  # the given store_item, or else it returns nil.
-  def get_receipt_item(receipt, store_item)
-    if receipt.nil? or store_item.nil?
-      return nil
-    end
-
-    receipt_items = receipt.receipt_items
-    found_item = nil
-
-    receipt_items.each do |item| 
-      if item.store_item.name == store_item.name 
-        found_item = item
-      end
-    end
-
-    found_item
-  end
 
   def receipt_params
     params.require(:receipt).permit(:notes, :date_purchased, :store_id)
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -5,4 +5,47 @@ class Receipt < ActiveRecord::Base
   has_many :receipt_items, dependent: :destroy
   validates :total, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10000000}
   validates :notes, length: { maximum: 2000 }
+
+  def find_store_item(store_item)
+    # Given a store_item it looks through the receipt for a receipt_item that has
+    # the given store_item, or else it returns nil.
+    if store_item.nil?
+      return nil
+    end
+
+    receipt_items = self.receipt_items
+    found_item = nil
+
+    receipt_items.each do |item| 
+      if item.store_item.name == store_item.name 
+        found_item = item
+      end
+    end
+
+    found_item
+  end
+
+  def update_quantity_and_price(store_items, items_to_update, new_receipt_items, params)
+    store_items.each do |store_item|
+      same_receipt_item = self.find_store_item(store_item)
+      if same_receipt_item
+        same_receipt_item.quantity += params["quantity_item_#{store_item.id}"].to_f
+
+        same_receipt_item.save!
+        items_to_update.append({item_id: same_receipt_item.id, quantity: same_receipt_item.quantity})
+      else
+        @new_receipt_item = ReceiptItem.new(quantity: params["quantity_item_#{store_item.id}"], 
+                                            price: params["store_item_price_#{store_item.id}"], 
+                                            store_item: store_item,
+                                            receipt: @receipt)
+
+        if @new_receipt_item.valid?
+          new_receipt_items.push(@new_receipt_item) 
+        else
+          return false
+        end
+      end
+    end
+  end
+
 end
